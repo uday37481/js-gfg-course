ace5bc94f14968f47b4cbc84b8ebbd955de55561
uday37481 <uvnagar370823@kkwagh.edu.in>
2025-06-20 08:15:20 +0000

    add initial JavaScript program to log messages to the console

---
+++ 01_basics/variable.js
@@ -0,0 +1,28 @@
+/*
+  First Program - Show hello geeks in console 
+  Log - to write something somewhere
+  Log this text on the console
+  Console is an API provided by the browser
+  This is a simple JavaScript program that prints "Hello Geeks" to the console.
+*/
+
+console.log('Hello Geeks');
+console.log(14*2);
+
+var msg;
+msg = "Hello GeeksforGeeks";
+console.log(msg);
+console.log(msg);
+console.log(msg);
+console.log(msg);
+
+
+let text;                         //creating declaring a var
+text = "Javascript is the best !!!!";    // assigning a value to the variable
+console.log(text);
+
+// let text = "Javascripts is the best !!!!";  
+// console.log(text);
+
+const num = 20 ;
+console.log(num);
