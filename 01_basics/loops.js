de082129ce26ad1bc7a2392e4a6df9ecc65f1be1
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-06-22 21:10:39 +0530

    Create loops.js
    This file contains JS concepts of looping .

---
+++ 01_basics/loops.js
@@ -0,0 +1,30 @@
+//                                                 for loop
+var i;
+for (i = 0; i < 10; i++)
+{
+    console.log("Vamosss !!");
+}
+
+
+//                                           while loop
+var a = 1;
+while(a <= 10)
+{
+    console.log("It's to easssy!");
+    a++;
+}
+
+
+//                                           do-while loop
+let j = 0 ;
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+console.log();
+
+//                                         Printing Characters of a String
+const name = "Mamta Banerjee";
+for (let k = 0; k < name.length; k++) {
+    console.log(name[k]);
+}
